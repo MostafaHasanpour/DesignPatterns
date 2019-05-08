5f98ea46056b7dbb0d34dbe3e6cf6ab80ad29d38
Mostafa Hasanpour <developer.hasanpour@outlook.com>
2019-05-09 00:06:32 +0430

    singleton pattern

---
+++ DesignPattern.Creational.SingletonPattern/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DesignPattern.Creational.SingletonPattern
+{
+    public  sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+
+        private int numberOfInstances = 0;
+
+        //Private constructor is used to prevent
+        //creation of instances with 'new' keyword outside this class
+        private Singleton()
+        {
+            Console.WriteLine("Instantiating inside the private constructor.");
+            numberOfInstances++;
+            Console.WriteLine("Number of instances ={0}", numberOfInstances);
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("We already have an instance now.Use it.");
+                return instance;
+            }
+        }
+
+
+        public static void CreateString(string txt)
+        {
+            MySingletonString = txt;
+        }
+
+        public static string MySingletonString { get; private set; }
+        public static int MyInt = 25;
+
+    }
+}
